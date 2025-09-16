69a9891c1a8f7c87e99f2ec9113f361efb2001fc
Ivan Trajanovski <ivantrajanovski@gmail.com>
2025-09-16 12:52:51 +0200

    Initial Commit

---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by Ivan Trajanovski  on 16.09.25.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
